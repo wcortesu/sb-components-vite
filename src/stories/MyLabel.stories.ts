85c3f635e8e0b7ffd3674ddeb2bceda54c9fecde
wcortesu <wcortesu@gmail.com>
2023-11-20 06:59:01 -0500

    Storybook ready

---
+++ src/stories/MyLabel.stories.ts
@@ -0,0 +1,46 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { MyLabel } from "../components/MyLabel";
+
+const meta = {
+  title: "IU/labels/MyLabel",
+  component: MyLabel,
+  tags: ['autodocs'],
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    size: { control: 'inline-radio' },
+    allCaps: { control: 'boolean' },
+    fontColor: { control: 'color' }
+  }
+} satisfies Meta<typeof MyLabel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+  args: {
+    label: "Basic Label",
+  },
+};
+
+export const AllCaps: Story = {
+    args: {
+      label: "All Caps Label",
+      allCaps: true
+    },
+  };
+
+  export const Secondary: Story = {
+    args: {
+      label: "Secondary Label",
+      color: 'text-secondary'
+    },
+  };
+
+  export const CustomColor: Story = {
+    args: {
+      label: "Custom Color Label",
+      fontColor: '#5517ac'
+    },
+  };
